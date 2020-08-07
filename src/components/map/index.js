23bc2366ec7bdfc7cb882d885ec331d36aa40276
unknown <jethanhargrave@gmail.com>
2020-08-06 23:35:02 -0400

    FIXED: Popup width overflow & links

--- src/components/map/index.js
+++ src/components/map/index.js
@@ -1,30 +1,46 @@
-import React, { useState, useEffect, useRef } from 'react';
-import MapGL, {
-  Marker,
-  Popup,
-  NavigationControl,
-  FlyToInterpolator,
-} from 'react-map-gl';
-
-import Geocoder from 'react-map-gl-geocoder';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import 'react-map-gl-geocoder/dist/mapbox-gl-geocoder.css';
+import React, { useState, useRef } from 'react';
+import MapGL, { Marker, Popup } from 'react-map-gl';
 import DeckGL, { GeoJsonLayer } from 'deck.gl';
-
+import Geocoder from 'react-map-gl-geocoder';
 import * as pbdb from '../../data/846db.json';
 
-export default function Map() {
+function App() {
+  /////////////////////////////////////////////////////////////////////////////////// STATE
   const [viewport, setViewport] = useState({
     latitude: 41.4211,
     longitude: -74.6903,
-    width: '100vw',
-    height: '100vh',
+    width: '75vw',
+    height: '75vh',
     zoom: 4,
   });
-
+  const [selectedStation, setSelectedStation] = useState(null);
   const [searchResultLayer, setSearchResultsLayer] = useState(null);
+
+  ///////////////////////////////////////////////////////////////////////////////////  REF
   const mapRef = useRef();
+  const geocoderContainerRef = useRef();
+
+  /////////////////////////////////////////////////////////////////////////////////// HANDLERS
+  const handleViewportChange = newViewport => {
+    setViewport({ ...viewport, ...newViewport });
+  };
+
+  const handleMarkerChange = newViewport => {
+    const geocoderDefaultOverrides = { transitionDuration: 1000 };
+    setViewport({ ...viewport, ...newViewport, ...geocoderDefaultOverrides });
+  };
+
+  const handleGeocoderViewportChange = viewport => {
+    const geocoderDefaultOverrides = { transitionDuration: 6500 };
+    return handleViewportChange({
+      ...viewport,
+      ...geocoderDefaultOverrides,
+    });
+  };
 
   const handleOnResult = event => {
-    console.log(event.result);
     setSearchResultsLayer(
       new GeoJsonLayer({
         id: 'search-result',
@@ -36,106 +52,94 @@ export default function Map() {
       })
     );
   };
-  const [selectedStation, setSelectedStation] = useState(null);
-
-  useEffect(() => {
-    const listener = e => {
-      if (e.key === 'Escape') {
-        setSelectedStation(null);
-      }
-    };
-    window.addEventListener('keydown', listener);
-
-    return () => {
-      window.removeEventListener('keydown', listener);
-    };
-  }, []);
-  // const _goToNYC = (e) => {
-  //   const viewport = {
-  //     ...viewport,
-  //     longitude: -74.1,
-  //     latitude: 40.7,
-  //     zoom: 14,
-  //     transitionDuration: 5000,
-  //     transitionInterpolator: new FlyToInterpolator(),
-  //   };
-  //   setViewport({ viewport });
-  // };
 
   return (
-    <div style={{ height: '100vh' }}>
-      <MapGL
-        ref={mapRef}
-        {...viewport}
-        width="100%"
-        height="100%"
-        onViewportChange={viewport => {
-          setViewport(viewport);
-        }}
-        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-        mapStyle="mapbox://styles/mapbox/streets-v11"
-        // transitionDuration={1000}
-        transitionInterpolator={new FlyToInterpolator()}
-      >
-        {pbdb.data.map(station => (
-          <Marker
-            key={station.id}
-            longitude={parseFloat(station.geocoding.long)}
-            latitude={parseFloat(station.geocoding.lat)}
-          >
-            <button
-              className="marker-btn"
-              onClick={e => {
-                e.preventDefault();
-                setSelectedStation(station);
-              }}
-            >
-              <img
-                src="iconmap.png"
-                alt="map marker icon"
-                width="20px"
-                height="20px"
-                backgroundColor="black"
-              />
-            </button>
-          </Marker>
-        ))}
-        {selectedStation ? (
-          <Popup
-            latitude={parseFloat(selectedStation.geocoding.lat)}
-            longitude={parseFloat(selectedStation.geocoding.long)}
-            onClose={() => {
-              setSelectedStation(null);
-            }}
-            style="color: red;"
-          >
-            <div>
-              Incident:
-              <h2>{selectedStation.title}</h2>
-              <p>
-                {selectedStation.city}
-                <br></br> {selectedStation.state}
-                <br></br>
-                {selectedStation.date}
-                <br></br>
-                {selectedStation.links}
-                <br></br>
-                {selectedStation.description}
-              </p>
-            </div>
-          </Popup>
-        ) : null}
-        <Geocoder
-          mapRef={mapRef}
-          onResult={handleOnResult}
-          onViewportChange={viewport => {
-            setViewport(viewport);
+    <>
+      <div>
+        <div
+          ref={geocoderContainerRef}
+          style={{
+            height: 50,
+            display: 'flex',
+            alignItems: 'center',
           }}
-          mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-          position="top-left"
         />
-      </MapGL>
-      {/* <button onClick={_goToNYC}>New York City</button> */}
-    </div>
+        <MapGL
+          ref={mapRef}
+          {...viewport}
+          onViewportChange={handleViewportChange}
+          mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+          mapStyle="mapbox://styles/mapbox/streets-v11"
+        >
+          <Geocoder
+            mapRef={mapRef}
+            containerRef={geocoderContainerRef}
+            onResult={handleOnResult}
+            onViewportChange={handleGeocoderViewportChange}
+            mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+          />
+          <DeckGL {...viewport} layers={[searchResultLayer]}>
+            {pbdb.data.map(station => (
+              <Marker
+                key={station.id}
+                longitude={parseFloat(station.geocoding.long)}
+                latitude={parseFloat(station.geocoding.lat)}
+              >
+                <button
+                  className="marker-btn"
+                  onClick={e => {
+                    e.preventDefault();
+                    setSelectedStation(station);
+                    handleMarkerChange({
+                      zoom: 11,
+                      latitude: parseFloat(station.geocoding.lat),
+                      longitude: parseFloat(station.geocoding.long),
+                    });
+                  }}
+                >
+                  <img
+                    src="iconmap.png"
+                    alt="map marker icon"
+                    width="20px"
+                    height="20px"
+                    backgroundColor="black"
+                  />
+                </button>
+              </Marker>
+            ))}
+            {selectedStation ? (
+              <Popup
+                latitude={parseFloat(selectedStation.geocoding.lat)}
+                longitude={parseFloat(selectedStation.geocoding.long)}
+                onClose={() => {
+                  setSelectedStation(null);
+                }}
+                closeOnClick={false}
+              >
+                <div>
+                  <h2>{selectedStation.title}</h2>
+                  <h3>
+                    {selectedStation.city}, {selectedStation.state}
+                  </h3>
+                  <p>{selectedStation.date}</p>
+                  {selectedStation.links.map(link => (
+                    <p>
+                      Link:{' '}
+                      <a href={link}>
+                        <br></br>
+                        {link}
+                      </a>
+                    </p>
+                  ))}
+                  <p>{selectedStation.description}</p>
+                </div>
+              </Popup>
+            ) : null}
+          </DeckGL>
+        </MapGL>
+      </div>
+    </>
   );
 }
+
+export default App;
