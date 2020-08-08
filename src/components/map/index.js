dfe4271fa8dabf6b5859299c5ff890e95b2f9a69
unknown <jethanhargrave@gmail.com>
2020-08-07 23:43:04 -0400

    FIXED: Random scrambling of markers on map re-rendering

--- src/components/map/index.js
+++ src/components/map/index.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   GoogleMap,
   withScriptjs,
   withGoogleMap,
   Marker,
+  InfoWindow,
 } from 'react-google-maps';
+
 import * as pbdb from '../../data/846db.json';
 
 const noise = coord => {
@@ -12,30 +14,68 @@ const noise = coord => {
   return parseFloat(coord) + scale * (Math.random() - 0.5);
 };
 
+const newData = pbdb.data.map(item => {
+  const x = item.geocoding.lat;
+  const y = item.geocoding.long;
+  return {
+    ...item,
+    geocoding: {
+      lat: parseFloat(x) + 2 * 0.05 * (Math.random() - 0.5),
+      long: parseFloat(y) + 2 * 0.05 * (Math.random() - 0.5),
+    },
+  };
+});
+
+console.log(newData);
+
 function Map() {
+  const [selectedIncident, setSelectedIncident] = useState(null);
   return (
     <GoogleMap
-      defaultZoom={10}
-      defaultCenter={{ lat: 45.421532, lng: -75.697189 }}
+      defaultZoom={4.5}
+      defaultCenter={{ lat: 40.98832580000001, lng: -102.26435190000001 }}
     >
-      {pbdb.data.map(incident => (
+      {newData.map(incident => (
         <Marker
           key={incident.id}
           position={{
-            lat: noise(incident.geocoding.lat),
-            lng: noise(incident.geocoding.long),
+            lat: parseFloat(incident.geocoding.lat),
+            lng: parseFloat(incident.geocoding.long),
+          }}
+          onClick={() => {
+            setSelectedIncident(incident);
           }}
         ></Marker>
       ))}
+      {selectedIncident && (
+        // <InfoWindow
+        //   position={{
+        //     lat: parseFloat(selectedIncident.geocoding.lat),
+        //     lng: parseFloat(selectedIncident.geocoding.long),
+        //   }}
+        //   onCloseClick={() => {
+        //     setSelectedIncident(null);
+        //   }}
+        // >
+        <div>
+          <h2>Incident Information:</h2>
+          <h3>{selectedIncident.title}</h3>
+          <p>
+            Location: {selectedIncident.city}, {selectedIncident.state}{' '}
+          </p>
+          <p>Date:{selectedIncident.date}</p>
+          <p></p>
+          <p></p>
+        </div>
+        // </InfoWindow>
+      )}
     </GoogleMap>
   );
 }
-
 const WrappedMap = withScriptjs(withGoogleMap(Map));
-
 export default function App() {
   return (
-    <div style={{ width: '100vw', height: '100vh' }}>
+    <div style={{ width: '75vw', height: '75vh' }}>
       <WrappedMap
         googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyCdgQinpBF_rWLJIJJzG9ZhiXDuHtTzz8U`}
         loadingElement={<div style={{ height: '100%' }} />}
